203555c6c7af86ad8c34ea69bf53181d8a69dc3c
x1a0t <405028157@qq.com>
2023-03-28 16:19:44 +0800

    chore: ci

--- scripts/push-tag.sh
+++ scripts/push-tag.sh
@@ -7,4 +7,4 @@ podspec_path="./octobase.podspec" &&
 version=$(awk -F'"' '/spec.version/ {print $2; exit;}' "${podspec_path}") &&
 version_with_prefix="swift-v${version}" &&
 git tag "${version_with_prefix}" &&
-git push https://github.com/toeverything/octobase-swift-binding.git "${version_with_prefix}"
+git push https://github.com/thorseraq/octobase-swift-binding.git "${version_with_prefix}"
