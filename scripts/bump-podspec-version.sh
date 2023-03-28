8b9f8904dc303cf185250b37a6848fce53b71767
x1a0t <405028157@qq.com>
2023-03-28 15:51:11 +0800

    chore: ci

--- scripts/bump-podspec-version.sh
+++ scripts/bump-podspec-version.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 echo "Bumping version in octobase.podspec..."
 
-pwd
-podspec=$(cat ../octobase.podspec)
+podspec=$(cat ./octobase.podspec)
 current_version=$(echo "$podspec" | grep -oE 'spec\.version\s*=\s*"[0-9]+\.[0-9]+\.[0-9]+"' | awk -F'"' '{print $2}')
 
 IFS='.' read -ra version_parts <<< "$current_version"
@@ -12,4 +11,4 @@ patch=$((version_parts[2] + 1))
 
 new_version="$major.$minor.$patch"
 new_podspec=$(echo "$podspec" | sed "s/$current_version/$new_version/")
-echo "$new_podspec" > ../octobase.podspec
+echo "$new_podspec" > ./octobase.podspec
